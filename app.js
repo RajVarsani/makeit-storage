71c4cec66c411530e56c10c8a6c3d05ba218e69d
Raj <varasaniraj5@gmail.com>
2022-02-12 00:21:12 +0530

    Created image uploading service - Updated gitignore - Removed package-lock

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
 require("dotenv").config();
+var cloudinary = require("cloudinary").v2;
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs = require("fs");
+const { promisify } = require("util");
+const unlinkAsync = promisify(fs.unlink);
 const app = express();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
 var cors = require("cors");
 app.use(cors());
-
-app.listen(process.env.PORT || 8000, () => {
-  console.log("Server Connceted");
-});
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Connected to port ${process.env.PORT || 8000}`);
+});
+
 app.get("/", (req, res) => {
-  res.send(`Server is running }`);
+  res.send(`Server is up and running`);
+});
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+app.post("/uploadImages", upload.array("photos"), async (req, res) => {
+  let images = {};
+  let noOfUploads = req.files.length;
+  req.files.forEach(async (file, i) => {
+    try {
+      const result = await cloudinary.uploader.upload(file.path);
+      images[`${i}`] = result.url;
+      await unlinkAsync(file.path);
+    } catch {
+      images[`${i}`] = "";
+    }
+    noOfUploads--;
+    if (noOfUploads === 0) {
+      res.status(200).send(images);
+    }
+  });
 });
